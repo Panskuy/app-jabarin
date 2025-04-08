6eb89bcd1c333ce3a03c466d189dff6f71947884
Pandega <pandegarn@gmail.com>
2025-04-08 17:59:02 +0700

    tambah fitur

---
+++ src/app/not-found.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const NotFound = () => {
+  const router = useRouter();
+  return (
+    <div className="w-full max-w-7xl mx-auto flex flex-col justify-center items-center h-[900px]">
+      <h1 className="text-3xl mb-2 font-bold">Halaman tidak di temukan!</h1>
+      <button
+        onClick={() => router.back()}
+        className="bg-yellow-500 px-4 text-2xl py-2 rounded-lg"
+      >
+        Back
+      </button>
+    </div>
+  );
+};
+
+export default NotFound;
